e2482261b427533adcae53351f3c90ff73aeff20
Wanderson Morais <wandersonmoraisfsa@gmail.com>
2022-03-26 22:56:52 -0300

    criando endpoint cadastro de usuarios e integrando com banco de dados

--- src/index.js
+++ src/index.js
@@ -1,45 +1,34 @@
 import express from "express";
+import { openDatabase } from "./database.js";
 const app = express();
 
-const customers = [
-    {id: 1, name: 'Marcos Vinicius'},
-    {id: 2, name: 'Florinda', idade: 30}
-];
-
-app.use(express.json()); //antes de criar qualquer endpoint é preciso chamar essa função
-
-app.get('/api/request', (request, response) => {
-    response.send({
-        message: "response"
-    })
-});
-
-app.get('/api/customers', (request, response) => {
-    response.send({customers})
-});
-
-app.post('/api/customers', (request, response) => {
-    const { body } = request;
-    customers.push(body);
-    response.send({customers})
-});
-
-app.put('/api/customers/:id', (request, response) => {
-    const { id } = request.params; //pega o id da url
-    const { body } = request; // pega o corpo da requisição ou json
-
-    const index = customers.findIndex(customer => customer.id == id); //a função retorna a posição do array
-    customers[index] = {id, ...body};
-
-    response.send(customers);
-});
-
-app.delete('/api/customers/:id', (request, response) => {
-    const { id } = request.params; //pega o id da url
-    const newArray = customers.filter(customer => customer.id != id); //filtra todos os ids e pega o que foi solicitado
-    response.send(newArray);
-});
+app.use(express.json());
 
 app.listen(8000, () => {
     console.log("Servidor rodando na porta 8000...")
 });
+
+/* Endpoints users */
+
+app.get('/api/users', async (request, response) => {
+    
+    const db = await openDatabase();
+    const users = await db.all(
+        `SELECT * FROM users`
+
+    );
+    response.send(users)
+});
+
+app.post('api/users', (request, response) => {
+
+});
+
+app.put('api/users/:id', (request, response) => {
+
+});
+
+app.delete('api/users/:id', (request, response) => {
+
+});
+
